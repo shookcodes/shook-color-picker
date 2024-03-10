57b6deed63cce0e53f8bc2020dad35504f8816e0
Sarah Shook-Rowles <srowlesdeveloper@gmail.com>
2024-03-09 19:53:57 -0600

    Add input component & add copy/error handlers

--- src/utils/clipboard.ts
+++ src/utils/clipboard.ts
@@ -1,6 +1,9 @@
 const copyContent = async (text: string) => {
 	try {
 		await navigator.clipboard.writeText(text)
+
+		const res = 'copied!'
+		return res
 	} catch (err) {
 		console.error('Failed to copy: ', err)
 	}
